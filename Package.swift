2ba35d7e6bd1baa43aab749057eb4b5b65b13a28
Alan O'Connor <aloconnor@zendesk.com>
2020-12-07 14:20:33 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,8 +21,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZendeskAnswerBotProvidersSDK",
-            url: "https://github.com/zendesk/answer_bot_providers_sdk_ios/releases/download/2.1.2/AnswerBotProvidersSDK.xcframework.zip",
-            checksum: "584b60b74c14601873a40d8d244c9090d54afe10894306d9dcd1261792a7be75"
+            path: "AnswerBotProvidersSDK.xcframework"
         ),
         .target(name: "ZendeskSupportSDKTargets",
                 dependencies: [
