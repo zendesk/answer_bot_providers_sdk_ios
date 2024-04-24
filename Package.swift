783925f22968f536994c226c061367981110705d
runner <runner@zendesk.com>
2024-04-24 13:24:27 +0000

    [RELEASE] New version 5.0.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(name: "ZendeskSupportProvidersSDK",
                  url: "https://github.com/zendesk/support_providers_sdk_ios",
-                 .exact("8.0.0"))
+                 .exact("8.0.1"))
     ],
     targets: [
         .binaryTarget(
